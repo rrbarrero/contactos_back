f98fd877c56efc47d2cfd13bab75b66dd179dde9
Roberto <rbarrero@posteo.net>
2021-06-25 11:52:10 +0200

    Pasamos a pyenv y poetry

--- contactosapi/contactosapi/settings/devel.py
+++ contactosapi/contactosapi/settings/devel.py
@@ -10,32 +10,36 @@ DATABASES = {
         "PASSWORD": "rage",
         "HOST": "172.17.0.2",
         "PORT": "",
-        'TEST': {
-            'NAME': "test_contactosapi",
-            'CHARSET': 'utf8',
-            'COLLATION': 'utf8_general_ci',
+        "CHARSET": "utf8",
+        "COLLATION": "utf8_general_ci",
+        "TEST": {
+            "NAME": "test_contactosapi",
+            "CHARSET": "utf8",
+            "COLLATION": "utf8_general_ci",
         },
     },
 }
 
-INSTALLED_APPS += ("debug_toolbar", 'django_extensions',)
+INSTALLED_APPS += (
+    "debug_toolbar",
+    "django_extensions",
+)
 INTERNAL_IPS = ("127.0.0.1",)
 
-MIDDLEWARE += \
-    ("debug_toolbar.middleware.DebugToolbarMiddleware", )
+MIDDLEWARE += ("debug_toolbar.middleware.DebugToolbarMiddleware",)
 
 CORS_ORIGIN_WHITELIST = [
-    "http://localhost:4200", # "https://rbarrero.duckdns.org:442",
+    "http://localhost:4200",  # "https://rbarrero.duckdns.org:442",
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.TokenAuthentication',
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
     ],
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
     ],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 15
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
+    "PAGE_SIZE": 15,
 }
